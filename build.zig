53115482f43c2ae1b71ef8669ab3ef8b57e7483a
Mason Jones <20848827+smj-edison@users.noreply.github.com>
2026-05-16 19:27:29 -0600

    Get PCRE2 included correctly

--- build.zig
+++ build.zig
@@ -42,6 +42,18 @@ pub fn build(b: *std.Build) void {
         .linkage = .static,
     });
 
+    const pcre2_config = b.addWriteFiles().add("pcre2_config.h",
+        \\#define PCRE2_CODE_UNIT_WIDTH 8
+        \\#include "pcre2.h"
+    );
+    const pcre2_translated = b.addTranslateC(.{
+        .root_source_file = pcre2_config,
+        .target = target,
+        .optimize = optimize,
+    });
+    pcre2_translated.addIncludePath(pcre2.namedLazyPath("pcre2.h").dirname());
+    const pcre2_mod = pcre2_translated.createModule();
+
     // steps
     const run_step = b.step("run", "Run the application");
     const test_step = b.step("test", "Run all tests");
@@ -55,8 +67,8 @@ pub fn build(b: *std.Build) void {
 
     root.addImport("uucode", uucode.module("uucode"));
     root.addImport("options", options_mod);
+    root.addImport("pcre2", pcre2_mod);
     root.linkLibrary(pcre2.artifact("pcre2-8"));
-    root.addIncludePath(pcre2.namedLazyPath("pcre2.h").dirname());
 
     // executable
     const exe = b.addExecutable(.{
@@ -78,8 +90,8 @@ pub fn build(b: *std.Build) void {
     });
     lz_mod.addImport("uucode", uucode.module("uucode"));
     lz_mod.addImport("options", options_mod);
+    lz_mod.addImport("pcre2", pcre2_mod);
     lz_mod.linkLibrary(pcre2.artifact("pcre2-8"));
-    lz_mod.addIncludePath(pcre2.namedLazyPath("pcre2.h").dirname());
 
     const lz = b.addLibrary(.{
         .name = "zicl",
