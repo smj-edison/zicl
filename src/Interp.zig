9da953595755ebe9ae09cd0731229dc965a4ab9f
Mason Jones <20848827+smj-edison@users.noreply.github.com>
2026-05-22 21:02:10 -0600

    WIP: still crashing

--- src/Interp.zig
+++ src/Interp.zig
@@ -100,52 +100,16 @@ pub fn init() !void {
     variables = try objutil.newDict(Heap.local_heap, &.{});
 }
 
-/// [fn]
-pub fn fnCmd(args: []Handle) !void {
-    assert(args.len == 4);
-    const fn_name = &args[1];
-    const arglist = &args[2];
-    const body = args[3];
-
-    // Capture the current scope.
-    const scope = Interp.variables.borrow();
-
-    // Create the closure
-    const closure_handle = try Heap.local_heap.createObject();
-    const extra_data = try Heap.local_heap.createExtraData();
-
-    const closure: Heap.Closure = .{
-        .args = arglist.*,
-        .body = body,
-        .name = fn_name.toOptional(),
-        .scope = scope.toOptional(),
-        .required_arity = 1,
-        .cache_id = Heap.nextCacheId(),
-    };
-    Heap.local_heap.getExtraData(extra_data).* = .{ .closure = closure.borrow() };
-    closure_handle.peek().head.tag = .closure;
-    closure_handle.peek().body = .{ .closure = .{ .extra_data = extra_data } };
-
-    setVariable(fn_name.*, closure_handle.dupOrRef()) catch unreachable;
-}
-
 test "fn command" {
     _ = try Heap.testStart(testing.allocator, testing.io);
     try init();
     try registerCommand("fn");
 
-    const fn_str = try objutil.newString(Heap.local_heap, "fn");
-    const add_str = try objutil.newString(Heap.local_heap, "add");
-    const a_b_str = try objutil.newString(Heap.local_heap, "a b");
-    const fn1_body_str = try objutil.newString(Heap.local_heap, " + $a $b ");
-    var fn1_args: [4]Handle = .{ fn_str, add_str, a_b_str, fn1_body_str };
+    const foo_str = try objutil.newString(Heap.local_heap, "foo");
+    variables.incrRefCount();
+    try setVariable(foo_str, (try Heap.local_heap.createObject()).reference());
 
-    fnCmd(&fn1_args) catch unreachable;
-
-    const addx_str = try objutil.newString(Heap.local_heap, "add");
-    const a_str = try objutil.newString(Heap.local_heap, "a b");
-    const fn2_body_str = try objutil.newString(Heap.local_heap, " + $a $x ");
-    var fn2_args: [4]Handle = .{ fn_str, addx_str, a_str, fn2_body_str };
-
-    fnCmd(&fn2_args) catch unreachable;
+    const bar_str = try objutil.newString(Heap.local_heap, "bar");
+    variables.incrRefCount();
+    try setVariable(bar_str, (try Heap.local_heap.createObject()).reference());
 }
